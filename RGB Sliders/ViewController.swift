ff06368ce71ba869c594d444ce0c247972d20031
Vince Day <vince@vday-fs.fios-router.home>
2015-08-20 17:04:30 -0400

    Storing user preferences

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -19,14 +19,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let defaults = NSUserDefaults.standardUserDefaults()
+        redSlider.value = defaults.floatForKey("red")
+        greenSlider.value = defaults.floatForKey("green")
+        blueSlider.value = defaults.floatForKey("blue")
         colorSquare.layer.borderColor = UIColor.blackColor().CGColor
         colorSquare.layer.borderWidth = 1
         let red = CGFloat(redSlider.value)
         let green = CGFloat(greenSlider.value)
         let blue = CGFloat(blueSlider.value)
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
         
 
     
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "openColor")
+        {
+            let newViewController = segue.destinationViewController as! UIViewController
+            newViewController.view.backgroundColor = colorSquare.backgroundColor
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,8 +55,12 @@ class ViewController: UIViewController {
         let green = CGFloat(greenSlider.value)
         let blue = CGFloat(blueSlider.value)
         colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setFloat(redSlider.value, forKey: "red")
+        defaults.setFloat(greenSlider.value, forKey: "green")
+        defaults.setFloat(blueSlider.value, forKey: "blue")
+        defaults.synchronize()
 
-        // view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
         }
-    }
- // get git to work
+    } 
+
